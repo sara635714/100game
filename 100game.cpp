8782b2325a5e10f4fb465ecaebd69cbd32ee63d6
sara635714 <161848778+sara635714@users.noreply.github.com>
2024-03-09 08:27:40 +0200

    Update 100game.cpp

--- 100game.cpp
+++ 100game.cpp
@@ -1,12 +1,3 @@
-/*Program: Description of the program this game is a two players game they start from the point 0
- * and each of them will take turns adding a number from 1 to 10 and the one who reaches 100 fist is the winner
- * Game : game1(100game)
- * author : sara ibrahim mohamed
- * ID :20230166
- * version : 1.0
- * Date : 29/2/2024 */
-
-
 #include <iostream>
 #include <limits>
 
